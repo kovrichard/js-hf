aac5f36e309ea25e044c8e0a143c5a1846ce1a22
Richard Kovacs <richardkovacs.it@gmail.com>
2021-03-20 16:20:23 +0100

    get user mw skeleton and mock

--- middleware/user/getUser.js
+++ middleware/user/getUser.js
@@ -4,6 +4,17 @@
 module.exports = (objectRepository) => {
     return (req, res, next) => {
         console.log("Getting user...");
+        res.locals.user = getUserMock();
         next();
     }
 };
+
+// This will be deleted, once database is implemented
+function getUserMock() {
+    return {
+        id: 1,
+        username: 'admin',
+        name: 'Admin Admin',
+        email: 'admin@admin.com',
+    }
+}
