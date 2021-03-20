0025acf887130327eeb76645a3fe260dc1f8099e
Richard Kovacs <richardkovacs.it@gmail.com>
2021-03-20 16:34:00 +0100

    get user moview mw skeleton and mock

--- middleware/user/getUserMovies.js
+++ middleware/user/getUserMovies.js
@@ -4,6 +4,21 @@
 module.exports = (objectRepository) => {
     return (req, res, next) => {
         console.log("Getting movies rented by user...");
+        res.locals.usermovies = userMoviesMock();
         next();
     }
 };
+
+// This will be deleted, once database is implemented
+function userMoviesMock() {
+    return [
+        {
+            id: 2,
+            title: 'Avengers',
+        },
+        {
+            id: 1,
+            title: 'The Shawshank Redemption',
+        }
+    ]
+}
