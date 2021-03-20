c245abc60896fce499ffdee226ba1260af2189a0
Richard Kovacs <richardkovacs.it@gmail.com>
2021-03-20 15:33:29 +0100

    get movie mw skeleton and mock

--- middleware/movie/getMovie.js
+++ middleware/movie/getMovie.js
@@ -4,6 +4,26 @@
 module.exports = (objectRepository) => {
     return (req, res, next) => {
         console.log("Getting movie...");
+        res.locals.movie = getMovieMock();
         next();
     }
 };
+
+// This will be deleted, once database is implemented
+function getMovieMock() {
+    return {
+        id: 1,
+        title: 'The Shawshank Redemption',
+        directedby: 'Frank Darabont',
+        year: 1994,
+        category: 'Drama',
+        cast: [
+            'Tim Robbins',
+            'Morgan Freeman',
+            'Bob Gunton',
+            'William Sadler'
+        ],
+        image: '/static/the_shawshank_redemption.jpg',
+        available: 3,
+    }
+}
