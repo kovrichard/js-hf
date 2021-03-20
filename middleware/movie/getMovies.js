d53325ed1f68922f87502a16aa8e8ab354f0bcf7
Richard Kovacs <richardkovacs.it@gmail.com>
2021-03-20 15:42:26 +0100

    get movies mw skeleton and mock

--- middleware/movie/getMovies.js
+++ middleware/movie/getMovies.js
@@ -4,6 +4,37 @@
 module.exports = (objectRepository) => {
     return (req, res, next) => {
         console.log("Getting movies...");
+        res.locals.movies = getMoviesMock();
         next();
     }
 };
+
+// This will be deleted, once database is implemented
+function getMoviesMock() {
+    return [
+        {
+            id: 1,
+            title: 'The Shawshank Redemption',
+            directedby: 'Frank Darabont',
+            year: 1994,
+            category: 'Drama',
+            available: 3,
+        },
+        {
+            id: 2,
+            title: 'Avengers',
+            directedby: 'Joss Whedon',
+            year: 2012,
+            category: 'Superhero',
+            available: 2,
+        },
+        {
+            id: 3,
+            title: 'The Godfather',
+            directedby: 'Francis Ford Coppola',
+            year: 1972,
+            category: 'Crime',
+            available: 1,
+        },
+    ]
+}
