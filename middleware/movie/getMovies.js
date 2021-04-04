aa045d1b6ce0837793cf126efa7abec64db5c1b7
Richard Kovacs <richardkovacs.it@gmail.com>
2021-04-04 11:19:06 +0200

    get movies from db

--- middleware/movie/getMovies.js
+++ middleware/movie/getMovies.js
@@ -1,40 +1,20 @@
 /**
  * Load every movie from database
  */
+const requireOption = require('../generic/requireOption');
+
 module.exports = (objectRepository) => {
+    const MovieModel = requireOption(objectRepository, 'MovieModel');
+
     return (req, res, next) => {
         console.log("Getting movies...");
-        res.locals.movies = getMoviesMock();
-        next();
+        MovieModel.find({}, (err, movies) => {
+            if (err) {
+                return next(err);
+            }
+
+            res.locals.movies = movies;
+            return next();
+        });
     }
 };
-
-// This will be deleted, once database is implemented
-function getMoviesMock() {
-    return [
-        {
-            id: 1,
-            title: 'The Shawshank Redemption',
-            directedby: 'Frank Darabont',
-            year: 1994,
-            category: 'Drama',
-            available: 3,
-        },
-        {
-            id: 2,
-            title: 'Avengers',
-            directedby: 'Joss Whedon',
-            year: 2012,
-            category: 'Superhero',
-            available: 2,
-        },
-        {
-            id: 3,
-            title: 'The Godfather',
-            directedby: 'Francis Ford Coppola',
-            year: 1972,
-            category: 'Crime',
-            available: 1,
-        },
-    ]
-}
