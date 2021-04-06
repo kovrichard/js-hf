b7d2ca2dab0d9a6ca41f6de6b3901515d4bed5d3
Richard Kovacs <richardkovacs.it@gmail.com>
2021-04-06 12:46:51 +0200

    password checking with cookie creation

--- middleware/auth/checkPassword.js
+++ middleware/auth/checkPassword.js
@@ -5,21 +5,34 @@ const { request } = require("express");
  * If correct, redirect to /, if not, redirect to /login
  * If req.form is empty, call next() (we are in a GET request)
  */
+const requireOption = require('../generic/requireOption');
+
 module.exports = (objectRepository) => {
+    const UserModel = requireOption(objectRepository, 'UserModel');
+    
     return (req, res, next) => {
-        if (req.method == 'GET') {
-            console.log("GET request detected, calling next()...");
-            return next();
-        }
-
-        console.log(`Checking password for user ${req.body.username}`);
-
-        if (req.body.username == 'admin' && req.body.password == 'admin') {
-            console.log('Username and password OK, logging in...');
-            return res.redirect('/');
-        } else {
-            console.log('Username or password is wrong.');
+        if (typeof res.locals.user === 'undefined') {
             return res.redirect('/login');
         }
+
+        console.log(`Checking password for user ${res.locals.user.username}`);
+        UserModel.findOne({ username: res.locals.user.username },
+            (err, user) => {
+                if (err || !user) {
+                    return next(err);
+                }
+
+                if (req.body.password !== user.password) {
+                    console.log("Password is wrong");
+                    res.redirect('/login');
+                } else {
+                    console.log('Username and password OK, logging in...');
+                    res.cookie('userid', user._id.toString(), { maxAge: 24 * 60 * 60 * 1000 });
+                    // req.session.loggedin = true;
+                    // return req.session.save((err) => {
+                    // return res.redirect('/');
+                    return next();
+                }
+            });
     }
 };
