b5f9c982df0d10e679977f3e0da129d87bf33da2
Richard Kovacs <richardkovacs.it@gmail.com>
2021-04-06 14:48:37 +0200

    unrent mw

--- middleware/movie/unrentMovie.js
+++ middleware/movie/unrentMovie.js
@@ -4,7 +4,25 @@
  */
 module.exports = (objectRepository) => {
     return (req, res, next) => {
+        if (typeof res.locals.movie === 'undefined' || typeof res.locals.user === 'undefined') {
+            return res.redirect('/');
+        }
         console.log(`Unrenting movie ${res.locals.movie.title}`);
-        next();
+
+        res.locals.user._movies = res.locals.user._movies.filter(movie => movie != res.locals.movie.id);
+        res.locals.movie.available++;
+        res.locals.user.save((err) => {
+            if (err) {
+                return next(err);
+            }
+            res.locals.movie.save((saveMovieErr) => {
+                if (saveMovieErr) {
+                    return next(saveMovieErr);
+                }
+                console.log(`${res.locals.movie.title} was given back to the library.`);
+                return next();
+            });
+        });
+        return next();
     }
 };
--- routes/movie.js
+++ routes/movie.js
@@ -69,7 +69,6 @@ module.exports = (app) => {
         getUserMW(objectRepository),
         getMovieMW(objectRepository),
         unrentMovieMW(objectRepository),
-        saveUserMW(objectRepository),
         redirectMW(objectRepository, '/')
     );
 };
