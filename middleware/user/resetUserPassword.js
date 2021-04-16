ce4c6944b68686732dc01c23cd485c908d328fa6
Richard Kovacs <richardkovacs.it@gmail.com>
2021-04-16 21:06:07 +0200

    feat(mw): password reset implemented

--- middleware/user/resetUserPassword.js
+++ middleware/user/resetUserPassword.js
@@ -1,11 +1,24 @@
+const crypto = require('crypto');
+
 /**
  * Reset user password, if anything came in res.locals
  * Else call next() (we are in a GET request)
  */
 module.exports = (objectRepository) => {
     return (req, res, next) => {
+        if (typeof res.locals.user === 'undefined') {
+            return res.redirect('/password-reset');
+        }
+
         console.log(`Reseting password for user ${res.locals.user.username}`);
-        console.log('New password is admin');
-        next();
+        const passwd = crypto.randomBytes(10).toString('hex');
+        res.locals.user.password = passwd;
+        res.locals.user.save((err) => {
+            if (err) {
+                return next(err);
+            }
+            console.log(`New password is ${passwd}`);
+            return next();
+        });
     }
 };
