48ffdd7d973caa1e2c1c2c5424c2292993c62e7c
Richard Kovacs <richardkovacs.it@gmail.com>
2021-04-21 17:59:15 +0200

    test(mw): add tests for non-numeric inputs

--- test/unit/middleware/movie/saveMovie.js
+++ test/unit/middleware/movie/saveMovie.js
@@ -99,12 +99,79 @@ describe('saveMovie middleware ', () => {
                     expect(movieMock.image).to.be.eql('/static/testimage.jpg');
                     done();
                 }
-            },
-            (err) => {
-                // code does not call next
             }
         );
     });
 
-    
+    it('should render movie-modify on non-numeric year input', (done) => {
+        const mw = saveMovieMW({
+            MovieModel: 'movie'
+        });
+
+        mw(
+            {
+                body: {
+                    title: 'Movie 1',
+                    directedby: 'Director',
+                    year: 'wrong-year-format',
+                    category: 'Drama',
+                    cast: 'First,Second,Third',
+                    available: 4
+                }
+            },
+            {
+                locals: {
+                    movie: {
+                        save: (cb) => {
+                            cb(null);
+                        }
+                    }
+                },
+                render: (what, locals) => {
+                    expect(what).to.be.eql('movie-modify');
+                    expect(locals.movie.title).to.be.eql('Movie 1');
+                    expect(locals.movie.directedby).to.be.eql('Director');
+                    expect(locals.movie.category).to.be.eql('Drama');
+                    expect(locals.movie.available).to.be.eql(4);
+                    done();
+                }
+            }
+        );
+    });
+
+    it('should render movie-modify on non-numeric available count input', (done) => {
+        const mw = saveMovieMW({
+            MovieModel: 'movie'
+        });
+
+        mw(
+            {
+                body: {
+                    title: 'Movie 1',
+                    directedby: 'Director',
+                    year: 2001,
+                    category: 'Drama',
+                    cast: 'First,Second,Third',
+                    available: 'four'
+                }
+            },
+            {
+                locals: {
+                    movie: {
+                        save: (cb) => {
+                            cb(null);
+                        }
+                    }
+                },
+                render: (what, locals) => {
+                    expect(what).to.be.eql('movie-modify');
+                    expect(locals.movie.title).to.be.eql('Movie 1');
+                    expect(locals.movie.directedby).to.be.eql('Director');
+                    expect(locals.movie.year).to.be.eql(2001);
+                    expect(locals.movie.category).to.be.eql('Drama');
+                    done();
+                }
+            }
+        );
+    });
 });
