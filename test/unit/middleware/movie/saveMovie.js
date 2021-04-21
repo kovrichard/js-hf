89b3eee6f41a368ba1871ca1fd5b6a3f1ca05303
Richard Kovacs <richardkovacs.it@gmail.com>
2021-04-21 18:44:12 +0200

    test(mw): add additional tests to savemovie mw

--- test/unit/middleware/movie/saveMovie.js
+++ test/unit/middleware/movie/saveMovie.js
@@ -121,11 +121,7 @@ describe('saveMovie middleware ', () => {
             },
             {
                 locals: {
-                    movie: {
-                        save: (cb) => {
-                            cb(null);
-                        }
-                    }
+                    movie: {}
                 },
                 render: (what, locals) => {
                     expect(what).to.be.eql('movie-modify');
@@ -157,11 +153,7 @@ describe('saveMovie middleware ', () => {
             },
             {
                 locals: {
-                    movie: {
-                        save: (cb) => {
-                            cb(null);
-                        }
-                    }
+                    movie: {}
                 },
                 render: (what, locals) => {
                     expect(what).to.be.eql('movie-modify');
@@ -174,4 +166,105 @@ describe('saveMovie middleware ', () => {
             }
         );
     });
+
+    it('should render movie-modify on empty title input', (done) => {
+        const mw = saveMovieMW({
+            MovieModel: 'movie'
+        });
+
+        mw(
+            {
+                body: {
+                    title: '',
+                    directedby: 'Director',
+                    year: 2001,
+                    category: 'Drama',
+                    cast: 'First,Second,Third',
+                    available: 4
+                }
+            },
+            {
+                locals: {
+                    movie: {}
+                },
+                render: (what, locals) => {
+                    expect(what).to.be.eql('movie-modify');
+                    expect(locals.movie.title).to.be.eql('');
+                    expect(locals.movie.directedby).to.be.eql('Director');
+                    expect(locals.movie.year).to.be.eql(2001);
+                    expect(locals.movie.category).to.be.eql('Drama');
+                    expect(locals.movie.available).to.be.eql(4);
+                    done();
+                }
+            }
+        );
+    });
+
+    it('should render movie-modify on empty available count input', (done) => {
+        const mw = saveMovieMW({
+            MovieModel: 'movie'
+        });
+
+        mw(
+            {
+                body: {
+                    title: 'Movie 1',
+                    directedby: 'Director',
+                    year: 2001,
+                    category: 'Drama',
+                    cast: 'First,Second,Third',
+                    available: ''
+                }
+            },
+            {
+                locals: {
+                    movie: {}
+                },
+                render: (what, locals) => {
+                    expect(what).to.be.eql('movie-modify');
+                    expect(locals.movie.title).to.be.eql('Movie 1');
+                    expect(locals.movie.directedby).to.be.eql('Director');
+                    expect(locals.movie.year).to.be.eql(2001);
+                    expect(locals.movie.category).to.be.eql('Drama');
+                    done();
+                }
+            }
+        );
+    });
+
+    it('should save movie cast as list', (done) => {
+        const mw = saveMovieMW({
+            MovieModel: 'movie'
+        });
+
+        var movieMock = {
+            save: (cb) => {
+                cb(null);
+            }
+        }
+
+        mw(
+            {
+                body: {
+                    title: 'Movie 1',
+                    directedby: 'Director',
+                    year: 2001,
+                    category: 'Drama',
+                    cast: 'First,Second,Third',
+                    available: 4
+                }
+            },
+            {
+                locals: {
+                    movie: movieMock
+                },
+                redirect: (where) => {
+                    expect(movieMock.cast).to.be.eql(['First', 'Second', 'Third']);
+                    done();
+                }
+            }
+        );
+    });
+
+    
 });
